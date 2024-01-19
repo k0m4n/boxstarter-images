c1a6c41a596b1dcef8a8075b3022818a68286602
k0m4n <20258285+k0m4n@users.noreply.github.com>
2024-01-19 12:07:20 -0800

    fixed pworsehll command to launch boxstarter

--- k0m4n-win11-client.bat
+++ k0m4n-win11-client.bat
@@ -1,3 +1,5 @@
+@echo off
+pushd %~dp0
 winget install Chocolatey.Chocolatey --accept-source-agreements --accept-package-agreements
 choco install boxstarter -y
-START https://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/k0m4n/boxstarter-images/main/k0m4n-win11-client.ps1
+powershell "Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/k0m4n/boxstarter-images/main/k0m4n-win11-client.ps1"
