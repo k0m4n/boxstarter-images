74e591ef4c48887cb4b077893a7ed91b4c443d3e
k0m4n <20258285+k0m4n@users.noreply.github.com>
2024-01-19 12:10:08 -0800

    no reboot on choco install

--- k0m4n-win11-client.bat
+++ k0m4n-win11-client.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0
 winget install Chocolatey.Chocolatey --accept-source-agreements --accept-package-agreements
-choco install boxstarter -y
+choco install boxstarter -y --ignoredetectedreboot
 powershell "Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/k0m4n/boxstarter-images/main/k0m4n-win11-client.ps1"
