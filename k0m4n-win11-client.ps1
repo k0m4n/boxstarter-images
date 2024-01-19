352ad528432a14c2002ba75c36f2c20a08597038
k0m4n <20258285+k0m4n@users.noreply.github.com>
2024-01-19 11:30:35 -0800

    k0m4n-win11-client.ps1

---
+++ k0m4n-win11-client.ps1
@@ -0,0 +1,23 @@
+winget install Chocolatey.Chocolatey --accept-source-agreements --accept-package-agreements
+winget install XP8C9QZMS2PC1T --accept-source-agreements --accept-package-agreements
+winget install Discord.Discord --accept-source-agreements --accept-package-agreements
+winget install Proton.ProtonDrive --accept-source-agreements --accept-package-agreements
+winget install ProtonTechnologies.ProtonVPN --accept-source-agreements --accept-package-agreements
+winget install OpenVPNTechnologies.OpenVPNConnect --accept-source-agreements --accept-package-agreements
+winget install XP89DCGQ3K6VLD --accept-source-agreements --accept-package-agreements
+winget install 9N8GNLC8Z9C8 --accept-source-agreements --accept-package-agreements
+winget install TheDocumentFoundation.LibreOffice --accept-source-agreements --accept-package-agreements
+winget install Valve.Steam --accept-source-agreements --accept-package-agreements
+winget install EpicGames.EpicGamesLauncher --accept-source-agreements --accept-package-agreements
+winget install 9PPF5MZPHBWB --accept-source-agreements --accept-package-agreements
+winget install Corsair.iCUE.5 --accept-source-agreements --accept-package-agreements
+winget install Asus.ArmouryCrate --accept-source-agreements --accept-package-agreements
+winget install Yubico.YubikeyManager --accept-source-agreements --accept-package-agreements
+winget install 9NFNG39387K0 --accept-source-agreements --accept-package-agreements
+winget install 9NP83LWLPZ9K --accept-source-agreements --accept-package-agreements
+winget install XP8BT8DW290MPQ --accept-source-agreements --accept-package-agreements
+winget install 9P3395VX91NR --accept-source-agreements --accept-package-agreements
+winget upgrade --all
+wsl --install kali-linux --no-launch
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -All -Online
+Enable-WindowsOptionalFeature -FeatureName Containers-DisposableClientVM -All -Online
