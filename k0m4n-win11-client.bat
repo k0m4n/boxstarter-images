d6c749ccd03190f1fd7018e7769118802ea6898e
k0m4n <20258285+k0m4n@users.noreply.github.com>
2024-01-19 11:32:53 -0800

    k0m4n-win11-client.bat

---
+++ k0m4n-win11-client.bat
@@ -0,0 +1,3 @@
+winget install Chocolatey.Chocolatey --accept-source-agreements --accept-package-agreements
+choco install boxstarter -y
+START https://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/k0m4n/boxstarter-images/main/k0m4n-win11-client.ps1
